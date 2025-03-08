3bf929f32e036cbc8b21958f4a8d4c321bf13b36
Prince Prabhakar <playhorn.pp@gmail.com>
2025-03-08 13:44:40 +0530

    added calculator functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+let workspace=document.querySelector("#workspace > p");
+
+let buttons=document.querySelectorAll("button");
+
+buttons.forEach((b)=>{
+    b.addEventListener("click",(e)=>{
+        
+        switch(b.id){
+            case "ac":  workspace.innerText=null;break;
+            case "c": workspace.innerText= workspace.innerText.slice(0,-1);break;
+            case "sign": changeSign(workspace.innerText);break;
+            case "divide": workspace.innerText= addThisOperatorTo('/',workspace.innerText);break;
+            case "multiply": workspace.innerText= addThisOperatorTo('x',workspace.innerText);break;
+            case "minus" : workspace.innerText= addThisOperatorTo('-',workspace.innerText);break;
+            case "plus" : workspace.innerText= addThisOperatorTo('+',workspace.innerText);break;
+            case "equal" :  workspace.innerText=total(workspace.innerText);break;
+            default : workspace.innerText+=b.innerText;
+        }
+        
+        });
+    });
+
+    function changeSign(str){
+        if(str.slice(-1)=='-'){
+            return workspace.innerText=workspace.innerText.slice(0,-1);
+        }
+        else
+            return workspace.innerText+="-";
+    }
+
+    function addThisOperatorTo(op,str){
+        if(isOperator(str.slice(-1)))
+            return str=str.slice(0,-1)+op;
+        else
+        return str=str+op;
+        
+    }
+
+    function isOperator(op){
+        if(op == '+' || op == '-' || op == 'x' || op == '/'){
+            return true;
+        }
+        return false;
+    }
+
+    function total(exp){
+        if(isOperator(exp.slice(-1)))
+            return exp;
+        exp=exp.replace('x','*');
+        return eval(exp);
+    }
